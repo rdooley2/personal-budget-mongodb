0a70696518a7cc9718aa4fcc21317869f38d641b
Ryan Dooley <rdooley2@charlotte.edu>
2024-09-09 15:53:56 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,30 +1,14 @@
 // Budget API
 
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 const app = express();
 const port = 3000;
+const budget = require("./server.json");
 
 app.use('/', express.static('public'));
 
-// app.use(cors());
-
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
+app.use(cors());
 
 app.get('/hello', (req, res) => {
     res.send("Hello World!");
