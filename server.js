2ee81d3b0803f04df79d2036614a9558ff77225c
Ryan Dooley <rdooley2@charlotte.edu>
2024-09-16 10:33:38 -0400

    Added CORS

--- server.js
+++ server.js
@@ -6,13 +6,7 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 
-app.use('/', express.static('public'));
-
 app.use(cors());
-
-app.get('/hello', (req, res) => {
-    res.send("Hello World!");
-});
 
 app.get('/budget', (req, res) => {
     // Dynamically read the server.json file on each request
